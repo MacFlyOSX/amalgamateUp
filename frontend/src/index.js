9fb1812c35758408083917e1d249444a0f9c2c2d
MacFlyOSX <b.tasaki@gmail.com>
2022-09-15 21:29:05 -0700

    added a login modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './index.css';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import configureStore from './store';
-import * as sessionActions from './store/session';
+// frontend/src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -21,9 +24,11 @@ if (process.env.NODE_ENV !== 'production') {
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
@@ -32,5 +37,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
