b617148ddd7d06ca04304c8912f8c92cd080b8df
MacFlyOSX <b.tasaki@gmail.com>
2022-09-15 21:15:43 -0700

    configured logout action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -50,6 +50,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
