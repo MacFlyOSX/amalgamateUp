c35f02108101a8073c8c7d4011661f7f6f323d92
MacFlyOSX <b.tasaki@gmail.com>
2022-09-16 11:17:15 -0700

    phase 1 of deployment to heroku complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,13 +9,6 @@ router.use('/api', apiRouter);
 //   res.send('Hello World!');
 // });
 
-// Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
-}
 
 // Static routes
 // Serve React build files in production
@@ -26,18 +19,26 @@ if (process.env.NODE_ENV === 'production') {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
-  });
+      );
+    });
 
-  // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/build")));
 
-  // Serve the frontend's index.html file at all other routes NOT starting with /api
-  router.get(/^(?!\/?api).*/, (req, res) => {
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+      });
+    }
+
+    // Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
+    return res.json({});
   });
 }
 
