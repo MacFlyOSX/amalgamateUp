a0b0db37e704331b45e3d2e1e964d908c88b0a0d
MacFlyOSX <b.tasaki@gmail.com>
2022-09-20 17:01:38 -0700

    moving onto the edit group form

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -9,32 +9,64 @@ import './GroupDetails.css';
 
 
 const GroupDetails = () => {
+    const sessionUser = useSelector(state => state.session.user);
     const { groupId } = useParams();
     const dispatch = useDispatch();
+    console.log('this is the session user', sessionUser);
+    const [popup, setPopup] = useState(false);
 
     const group = useSelector(state => state.groups[groupId]);
+
+    let sessionLinks;
+    if(sessionUser) {
+        setPopup(false);
+        if(sessionUser.id === group?.organizerId) {
+            sessionLinks = (
+                <div className='buttons-side-by-side'>
+                <button className='group-deets-button'>
+                        Edit this group
+                </button>
+                <button className='group-deets-button'>
+                        Delete this group
+                </button>
+                </div>
+            )
+        } else {
+            sessionLinks = (
+                <button className='group-deets-button'>
+                        Join this group
+                </button>
+            )
+        }
+    } else {
+            sessionLinks = (
+                <>
+                <span className={popup ? 'signin-popup show' : 'signin-popup'}>You must be signed in to join this group</span>
+                <div className='group-deets-div' onClick={() => setPopup(!popup)}>
+                    Join this group
+                </div>
+                </>
+            )
+    }
 
     useEffect(() => {
         dispatch(getOneGroup(groupId));
     }, [dispatch, groupId]);
 
-    if(!group) return null;
-    console.log(group);
-
     return (
         <div className='group-details-container'>
             <div className='top-section-group-details'>
-                <div className='group-main-image' style={{backgroundImage: `url(${group.previewImage})`}}>
+                <div className='group-main-image' style={{backgroundImage: `url(${group?.previewImage})`}}>
                     {/* <img class='group-thumbail' src={`${group.previewImage}`} alt='thumbnail' /> */}
                 </div>
                 <div className='group-information'>
-                    <h2 className='group-deets-name'>{group.name}</h2>
+                    <h2 className='group-deets-name'>{group?.name}</h2>
                     <div className='group-deets location'>
                     <div className='chunk'>
                         <img className='group-deets-icon' src={location} alt='location' />
                     </div>
                     <span>
-                        {`${group.city}, ${group.state}`}
+                        {`${group?.city}, ${group?.state}`}
                     </span>
                     </div>
                     <div className='group-deets members'>
@@ -42,7 +74,7 @@ const GroupDetails = () => {
                         <img className='group-deets-icon' src={members} alt='members' />
                     </div>
                     <span>
-                        {`${!!group.numMembers ? group.numMembers : 0} ${group.numMembers > 1 || !group.numMembers ? 'members' : 'member'} `} 	&middot; {!!group.private ? 'Private group' : 'Public group'}
+                        {`${!!group?.numMembers ? group?.numMembers : 0} ${group?.numMembers > 1 || !group?.numMembers ? 'members' : 'member'} `} 	&middot; {!!group?.private ? 'Private group' : 'Public group'}
                     </span>
                     </div>
                     <div className='group-deets organizer'>
@@ -50,12 +82,13 @@ const GroupDetails = () => {
                         <img className='group-deets-icon' src={organizer} alt='organizer' />
                     </div>
                     <span>
-                        Organized by {group.organizerName}
+                        Organized by {group?.organizerName}
                     </span>
                     </div>
-                    <button className='group-deets-join'>
+                    {sessionLinks}
+                    {/* <button className='group-deets-button'>
                         Join this group
-                    </button>
+                    </button> */}
                 </div>
             </div>
             <div className='group-details-bottom'>
@@ -64,7 +97,7 @@ const GroupDetails = () => {
                         What we're about
                     </h2>
                     <p className='group-deets-p'>
-                        {group.about}
+                        {group?.about}
                     </p>
                 </div>
                 <div className='group-deets-events'>
